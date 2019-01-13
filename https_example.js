c6ca945dbedf38765c70c249382d1755f9e0bb2a
trabnett <timrabnett@gmail.com>
2019-01-13 17:09:12 +0000

    read the data

--- https_example.js
+++ https_example.js
@@ -1,7 +1,5 @@
 var https = require('https');
 
-console.log('I did it!');
-
 var options = {
   host: 'www.example.org',
   path: '/'
@@ -10,7 +8,11 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('respose', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
